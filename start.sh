6f0157efb80eb73351c8b9a5bb0ddb5301e4b20a
rija244 <79934063+rija244@users.noreply.github.com>
2021-03-02 18:59:50 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.com:4250 -u DRhcwxMPz7tFKM2y8pMDLj8jNpSvJvLFbq -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zergpool.com:4250 -u DRhcwxMPz7tFKM2y8pMDLj8jNpSvJvLFbq -p c=DOGE
 
 
 done
